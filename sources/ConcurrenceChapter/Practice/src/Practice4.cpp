b4c83da1567186ad3d3286e423c859cb49477e97
xzm0307 <1061138927@qq.com>
2023-01-28 18:56:12 +0800

    compelete practice case

--- sources/ConcurrenceChapter/Practice/src/Practice4.cpp
+++ sources/ConcurrenceChapter/Practice/src/Practice4.cpp
@@ -19,7 +19,6 @@ void play_thread();
 
 void record_thread()
 {
-    std::thread t(play_thread);
     HANDLE          wait;
     waveform.wFormatTag = WAVE_FORMAT_PCM;//声音格式为PCM
     waveform.nSamplesPerSec = 8000;//采样率，16000次/秒
@@ -32,14 +31,14 @@ void record_thread()
     wait = CreateEvent(NULL, 0, 0, NULL);
     //使用waveInOpen函数开启音频采集
     waveInOpen(&hWaveIn, WAVE_MAPPER, &waveform, (DWORD_PTR)wait, 0L, CALLBACK_EVENT);
-    fopen_s(&pf, "test.pcm", "wb");
     
     //建立两个数组（这里可以建立多个数组）用来缓冲音频数据
     DWORD bufsize = 1024 * 100;//每次开辟10k的缓存存储录音数据
     int i = 20;
-    
+
     while (i--)//录制20左右秒声音，结合音频解码和网络传输可以修改为实时录音播放的机制以实现对讲功能
     {
+        fopen_s(&pf, "test.pcm", "wb");
         pBuffer1 = new BYTE[bufsize];
         wHdr1.lpData = (LPSTR)pBuffer1;
         wHdr1.dwBufferLength = bufsize;
@@ -55,12 +54,13 @@ void record_thread()
         fwrite(pBuffer1, 1, wHdr1.dwBytesRecorded, pf);
         delete pBuffer1;
         printf("%ds  ", i);
+        fclose(pf);
+        std::thread t(play_thread);
         if (t.joinable())
         {
             t.join();
         }
     }
-    fclose(pf);
     waveInClose(hWaveIn);
 }
 
@@ -81,17 +81,17 @@ void play_thread()
     wfx.wBitsPerSample = 16;
     wfx.cbSize = 0;//额外信息的大小
 
+    fopen_s(&thbgm, "test.pcm", "rb");
     while (true)
     {
         wait = CreateEvent(NULL, 0, 0, NULL);
         waveOutOpen(&hwo, WAVE_MAPPER, &wfx, (DWORD_PTR)wait, 0L, CALLBACK_EVENT);//打开一个给定的波形音频输出装置来进行回放
-        fopen_s(&thbgm, "test.pcm", "rb");
         if (thbgm == nullptr)
         {
             waveOutClose(hwo);
             return;
         }
-        cnt = fread(buf, sizeof(char), 1024 * 1024 * 4, thbgm);//读取文件4M的数据到内存来进行播放，通过这个部分的修改，增加线程可变成网络音频数据的实时传输。当然如果希望播放完整的音频文件，也是要在这里稍微改一改
+        cnt = fread(buf, sizeof(char), 1024, thbgm);//读取文件4M的数据到内存来进行播放，通过这个部分的修改，增加线程可变成网络音频数据的实时传输。当然如果希望播放完整的音频文件，也是要在这里稍微改一改
         if (cnt <= 0)
         {
             waveOutClose(hwo);
@@ -100,20 +100,23 @@ void play_thread()
         }
         int dolenght = 0;
         int playsize = 1024;
-        while (cnt) {//这一部分需要特别注意的是在循环回来之后不能花太长的时间去做读取数据之类的工作，不然在每个循环的间隙会有“哒哒”的噪音
-            wh.lpData = buf + dolenght;
-            wh.dwBufferLength = playsize;
-            wh.dwFlags = 0L;
-            wh.dwLoops = 1L;
-            waveOutPrepareHeader(hwo, &wh, sizeof(WAVEHDR));//准备一个波形数据块用于播放
-            waveOutWrite(hwo, &wh, sizeof(WAVEHDR));//在音频媒体中播放第二个函数wh指定的数据
-            WaitForSingleObject(wait, INFINITE);//用来检测hHandle事件的信号状态，在某一线程中调用该函数时，线程暂时挂起，如果在挂起的INFINITE毫秒内，线程所等待的对象变为有信号状态，则该函数立即返回
+        while (cnt > 0) {//这一部分需要特别注意的是在循环回来之后不能花太长的时间去做读取数据之类的工作，不然在每个循环的间隙会有“哒哒”的噪音
+            //wh.lpData = buf + dolenght;
+            //wh.dwBufferLength = playsize;
+            //wh.dwFlags = 0L;
+            //wh.dwLoops = 1L;
+            //waveOutPrepareHeader(hwo, &wh, sizeof(WAVEHDR));//准备一个波形数据块用于播放
+            //waveOutWrite(hwo, &wh, sizeof(WAVEHDR));//在音频媒体中播放第二个函数wh指定的数据
+            //WaitForSingleObject(wait, INFINITE);//用来检测hHandle事件的信号状态，在某一线程中调用该函数时，线程暂时挂起，如果在挂起的INFINITE毫秒内，线程所等待的对象变为有信号状态，则该函数立即返回
+            //std::cout << "play audio" << std::endl;
+
             dolenght = dolenght + playsize;
             cnt = cnt - playsize;
+            std::cout << cnt << std::endl;
         }
         waveOutClose(hwo);
-        fclose(thbgm);
     }
+    fclose(thbgm);
 }
 
 void CcPracticeRecordAndPlay(const char* msgIn, char* msgOut)
@@ -121,7 +124,6 @@ void CcPracticeRecordAndPlay(const char* msgIn, char* msgOut)
     FILE* file;
     fopen_s(&file, "test.pcm", "wb");
     fclose(file);
-    std::cout << "123" << std::endl;
     std::thread t(record_thread);
     t.join();
     return;
